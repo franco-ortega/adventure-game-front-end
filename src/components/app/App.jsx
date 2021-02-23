518ce5e747c5509793212a77eea3f0016058b36f
Franco Ortega <francopdx@gmail.com>
2021-02-23 12:20:50 -0800

    set up initial App with Router and Home component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from '../home/Home';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Save the Farm!</h1>
+      <Router>
+        <h3>Inside the Router</h3>
+        <Switch>
+          <Route
+            exact path="/"
+            component={Home}
+          />
+        </Switch>
+      </Router>
+    </>
+  );
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  console.log('Home component');
+  return (
+    <div>
+      You have found the Home Page.
+    </div>
+  );
+}
