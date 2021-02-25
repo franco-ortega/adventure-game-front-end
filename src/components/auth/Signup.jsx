54a9885e315753b9c7c1f8aae70928e3e5a82c03
Franco Ortega <francopdx@gmail.com>
2021-02-25 13:37:35 -0800

    refactor Signup to use AuthForm

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,36 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSignup } from '../../state/AuthContext';
+import AuthForm from './AuthForm';
 
 export default function Signup() {
   const signup = useSignup();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSignupSubmit = (e) => {
-    e.preventDefault();
-
-    signup(username, email, password);
-  };
-
-  return (
-    <form onSubmit={handleSignupSubmit}>
-      <input
-        type="text"
-        value={username}
-        placeholder="Username"
-        onChange={({ target }) => setUsername(target.value)} />
-      <input
-        type="email"
-        value={email}
-        placeholder="Email"
-        onChange={({ target }) => setEmail(target.value)} />
-      <input
-        type="password"
-        value={password}
-        placeholder="Password"
-        onChange={({ target }) => setPassword(target.value)} />
-      <button>Signup</button>
-    </form>
-  );
+  
+  return <AuthForm title="Signup" authFn={signup} />;
 }
