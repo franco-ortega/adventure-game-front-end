1c3a0c4ea321677f8b99595100e9a57931fe318e
Franco Ortega <francopdx@gmail.com>
2021-02-25 17:33:59 -0800

    add PrivateRoute component

---
+++ src/components/auth/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { useAuthLoading, useIsAuthenticated } from '../../state/AuthContext';
+
+export default function PrivateRoute(props) {
+  const isAuthenticated = useIsAuthenticated();
+  const authLoading = useAuthLoading();
+
+  if(authLoading) return <h1>Loading</h1>;
+
+  if(!authLoading && !isAuthenticated) {
+    return <Redirect to="/" />;
+  }
+
+  return <Route {...props} />;
+}
