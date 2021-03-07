1e8d3b3914d5d65c6d9aa4c54c94d4f8ded78311
Franco Ortega <francopdx@gmail.com>
2021-03-06 23:57:35 -0800

    add getCharacters to character.js service file

--- src/services/character.js
+++ src/services/character.js
@@ -1,5 +1,9 @@
-import { post } from './request';
+import { get, post } from './request';
 
 export const postCharacter = (character) => {
   return post('/api/v1/characters', character);
+};
+
+export const getCharacters = () => {
+  return get('/api/v1/characters');
 };
